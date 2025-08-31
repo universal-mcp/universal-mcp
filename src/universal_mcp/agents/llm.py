b0abeb68d637db1ebea7d1f07e0b66100a5986e2
Manoj Bajaj <manojbajaj95@gmail.com>
2025-08-30 20:37:16 -0700

    fix(agent): Fix agent streaming

--- src/universal_mcp/agents/llm.py
+++ src/universal_mcp/agents/llm.py
@@ -1,6 +1,6 @@
 from langchain_anthropic import ChatAnthropic
 from langchain_core.language_models import BaseChatModel
-from langchain_google_vertexai import ChatVertexAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_vertexai.model_garden import ChatAnthropicVertex
 from langchain_openai import AzureChatOpenAI
 
@@ -12,15 +12,32 @@ def load_chat_model(fully_specified_name: str, tags: list[str] | None = None) ->
     """
     provider, model = fully_specified_name.split("/", maxsplit=1)
     if provider == "google_anthropic_vertex":
-        return ChatAnthropicVertex(model=model, temperature=0.2, location="asia-east1", tags=tags)
+        return ChatAnthropicVertex(
+            model=model,
+            temperature=0.2,
+            location="asia-east1",
+            tags=tags,
+            stream_usage=True,
+        )
     elif provider == "anthropic":
         return ChatAnthropic(
-            model=model, temperature=1, thinking={"type": "enabled", "budget_tokens": 2048}, max_tokens=4096, tags=tags
+            model=model,
+            temperature=1,
+            thinking={"type": "enabled", "budget_tokens": 2048},
+            max_tokens=4096,
+            tags=tags,
+            stream_usage=True,
         )  # pyright: ignore[reportCallIssue]
     elif provider == "azure":
-        return AzureChatOpenAI(model=model, api_version="2024-12-01-preview", azure_deployment=model, tags=tags)
-    elif provider == "vertex":
-        return ChatVertexAI(model=model, temperature=0.1, tags=tags)
+        return AzureChatOpenAI(
+            model=model,
+            api_version="2024-12-01-preview",
+            azure_deployment=model,
+            tags=tags,
+            stream_usage=True,
+        )
+    elif provider == "gemini":
+        return ChatGoogleGenerativeAI(model=model, temperature=1.0, max_retries=2)
     else:
         raise ValueError(f"Unsupported provider: {provider}")
 
