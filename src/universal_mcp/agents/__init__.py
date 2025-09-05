f880f17043aa117c36432bdc937327d51cb31cf6
Ankit Ranjan <arstejas7@gmail.com>
2025-09-05 16:22:48 +0530

    chore: added BuilderAgent in __init__

--- src/universal_mcp/agents/__init__.py
+++ src/universal_mcp/agents/__init__.py
@@ -5,5 +5,6 @@ from universal_mcp.agents.planner import PlannerAgent
 from universal_mcp.agents.react import ReactAgent
 from universal_mcp.agents.simple import SimpleAgent
 from universal_mcp.agents.bigtool2 import BigToolAgent2
+from universal_mcp.agents.builder import BuilderAgent
 
-__all__ = ["BaseAgent", "ReactAgent", "SimpleAgent", "AutoAgent", "BigToolAgent", "PlannerAgent", "BigToolAgent2"]
+__all__ = ["BaseAgent", "ReactAgent", "SimpleAgent", "AutoAgent", "BigToolAgent", "PlannerAgent", "BuilderAgent" ,"BigToolAgent2"]
