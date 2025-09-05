527538248d4567b8c044ddae09c580464376f079
Ankit Ranjan <arstejas7@gmail.com>
2025-09-05 16:29:46 +0530

    rc13

--- src/universal_mcp/agents/__init__.py
+++ src/universal_mcp/agents/__init__.py
@@ -1,10 +1,9 @@
 from universal_mcp.agents.autoagent import AutoAgent
 from universal_mcp.agents.base import BaseAgent
 from universal_mcp.agents.bigtool import BigToolAgent
+from universal_mcp.agents.builder import BuilderAgent
 from universal_mcp.agents.planner import PlannerAgent
 from universal_mcp.agents.react import ReactAgent
 from universal_mcp.agents.simple import SimpleAgent
-from universal_mcp.agents.bigtool2 import BigToolAgent2
-from universal_mcp.agents.builder import BuilderAgent
 
-__all__ = ["BaseAgent", "ReactAgent", "SimpleAgent", "AutoAgent", "BigToolAgent", "PlannerAgent", "BuilderAgent" ,"BigToolAgent2"]
+__all__ = ["BaseAgent", "ReactAgent", "SimpleAgent", "AutoAgent", "BigToolAgent", "PlannerAgent", "BuilderAgent"]
