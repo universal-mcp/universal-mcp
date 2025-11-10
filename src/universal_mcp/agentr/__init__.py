d7b35e1fbddbc98cd3c7c7f53faa9bad010c5594
Manoj Bajaj <manojbajaj95@gmail.com>
2025-11-10 14:46:30 +0530

    fix: Move the agentr registry outside main repo

--- src/universal_mcp/agentr/__init__.py
+++
@@ -1,5 +0,0 @@
-from .client import AgentrClient
-from .integration import AgentrIntegration
-from .registry import AgentrRegistry
-
-__all__ = ["AgentrClient", "AgentrRegistry", "AgentrIntegration"]
