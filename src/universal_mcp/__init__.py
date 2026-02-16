12ed3287d85f85931887a89abe60af63392cf2a2
Manoj Bajaj <manojbajaj95@gmail.com>
2026-02-16 15:45:19 +0530

    Fix the universal mcp as namespace package

--- src/universal_mcp/__init__.py
+++ src/universal_mcp/__init__.py
@@ -1,3 +1,9 @@
+# Extend __path__ to enable namespace package behavior
+# This allows universal_mcp.applications to be discovered from multiple packages
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
 from universal_mcp.sdk import UniversalMCP
 
 __version__ = "2.0.0"
